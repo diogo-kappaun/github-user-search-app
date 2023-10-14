1470cba8858663b0f58f1d758758d9877a19c42d
diogo-kappaun <diogohkappaun@gmail.com>
2023-10-14 18:20:01 -0300

    :bug: fix: toggle-class-repeat-and-api-date-format

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,10 +1,34 @@
 export function toggleClassNotAvailable(data) {
-  const dataElement = document.querySelector(`.${data}-box`)
-  const textContent = document.querySelector(`.${data}-box #${data}`).textContent;
-  
-  if (textContent === 'Not Available') {
-    dataElement.classList.add('not-available')
+  const dataElement = document.querySelector(`.${data}-box`);
+  const textContent = document.querySelector(
+    `.${data}-box #${data}`
+  ).textContent;
+
+  if (textContent === "Not Available") {
+    dataElement.classList.add("not-available");
   } else {
-    dataElement.classList.remove('not-available')
+    dataElement.classList.remove("not-available");
   }
+}
+
+export function creationDayTreatment(createDate) {
+  const [day, month, year] = new Date(createDate)
+    .toLocaleDateString("pt-br")
+    .split("/");
+  const monthsIndex = Number(month);
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  return `Joined ${day} ${months[monthsIndex]} ${year}`;
 }
